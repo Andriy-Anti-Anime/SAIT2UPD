343efc606db8639f16dc6d3e151755d22e14a8e6
Max Slipchuk <maxslipchuk11@gmail.com>
2026-05-25 17:35:43 +0300

    lab 6

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('catalog/', views.catalog, name='catalog'),
     path('category/<slug:slug>/', views.category_detail, name='category_detail'),
+    path('product/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 
@@ -29,4 +29,14 @@ def catalog(request):
     return render(request, 'myapp/catalog.html', {
         'categories': categories,
         'products': products,
+    })
+
+
+def product_detail(request, slug):
+    categories = Category.objects.all()
+    product = get_object_or_404(Product, slug=slug)
+
+    return render(request, 'myapp/product_detail.html', {
+        'categories': categories,
+        'product': product
     })
